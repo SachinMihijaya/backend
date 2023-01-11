f39b983df9b178ab8050be1a65579cd8a782a516
Sachin <sachinmihijaya111@gmail.com>
2023-01-11 02:00:37 +0000

    january 11

--- main.py
+++ main.py
@@ -1,42 +1,39 @@
-import os
-import time
-import uuid0
-import shutil
-from keras.models import load_model
-from PIL import Image, ImageOps
+import io
+from fastapi import FastAPI,File,UploadFile   
+from fastapi.responses import StreamingResponse
+from PIL import Image,ImageOps
 import numpy as np
-from fastapi import FastAPI, UploadFile, File, status,BackgroundTasks
-
+import tensorflow as tf
+from keras_preprocessing.image import ImageDataGenerator
+from fastapi.middleware.cors import CORSMiddleware
+import keras_preprocessing
 
 app = FastAPI()
-# Load the model
-model = load_model('keras_model.h5')
 
-def delete_image(name):
-    time.sleep(30)
-    os.remove(name)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.post("/upload")
-async def upload_image(backgroundTasks:BackgroundTasks,file: UploadFile = File(...)):
-    ext = file.content_type.split('/')[1]
-    name = uuid0.generate().base62
-    with open(f"image/{name}.{ext}", "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+# load the save model
+model = tf.keras.models.load_model('keras_model.h5')
 
-    image = Image.open(f'image/{name}.{ext}').convert('RGB')
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+@app.post("/")
+async def upload_file(file:UploadFile):
+    content = await file.read()
+    image = Image.open(io.BytesIO(content)).convert('RGB')
+    image = ImageOps.fit(image, (224, 224), Image.ANTIALIAS)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
     #turn the image into a numpy array
     image_array = np.asarray(image)
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-    # Load the image into the array
-    data[0] = normalized_image_array
+    # expand tha array with another demention
+    test_image = np.expand_dims(image_array, axis=0)
 
-    # # run the inference
-    label = ['Class1','Class2']
-    prediction = model.predict(data)
-    backgroundTasks.add_task(delete_image,name=f'image/{name}.{ext}')
+    label = ['Aegypti','Albopictus','Other']
+    prediction = model.predict(test_image)
     return list(zip(label,prediction.tolist()[0]))
